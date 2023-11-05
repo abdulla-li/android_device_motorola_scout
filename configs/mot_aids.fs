25b776da387f680bb9ed7fe8d286206ee501ee21
Erfan Abdi <erfangplus@gmail.com>
2023-11-05 20:49:57 +0530

    scout: Import moto GIDs
    Change-Id: I4b05aed79e878a53315cdea0974ee0cd1c14ff22
    Signed-off-by: Notganesh <chimeports@gmail.com>

---
+++ configs/mot_aids.fs
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2017-2022 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+[AID_VENDOR_TCMD]
+value:2990
+
+[AID_VENDOR_APUSYS]
+value:2991
+
+[AID_VENDOR_PWRIC]
+value:2992
+
+[AID_VENDOR_PROD_PERSIST]
+value:2993
+
+[AID_VENDOR_FINGERP]
+value:2994
