bbd8562c906ab1416799a5835169867dc3669425
claxten10 <claxten10@gmail.com>
2025-04-22 12:31:06 +0200

    scout: Configure Media stack
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- device.mk
+++ device.mk
@@ -146,6 +146,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.mt6878.rc
 
+# Media
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/seccomp_policy/android.hardware.media.c2@1.2-mediatek-64b.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/android.hardware.media.c2@1.2-mediatek-64b.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
