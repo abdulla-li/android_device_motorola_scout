3e841b6e5c36bf0908ee830fbd4dca774a1b460b
claxten10 <claxten10@gmail.com>
2025-05-03 16:10:12 +0200

    scout: overlay: Configure inital brightness values
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- device.mk
+++ device.mk
@@ -245,6 +245,9 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+PRODUCT_PACKAGES += \
+    FrameworkResOverlayScout
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr \
