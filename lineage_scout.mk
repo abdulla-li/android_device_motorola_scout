df97f1d935deab246eea9d64bfa330d5d2692b83
Notganesh <chimeports@gmail.com>
2026-04-27 12:16:18 +0000

    scout: Update blobs to W1VCS36M.14-20-19-3
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- lineage_scout.mk
+++ lineage_scout.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from lyriq device
+# Inherit from scout device
 $(call inherit-product, device/motorola/scout/device.mk)
 
 # Inherit some common Lineage stuff.
@@ -20,3 +20,8 @@ PRODUCT_MODEL := motorola edge 60 fusion
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="scout_g_sys-user 16 W1VCS36M.14-20-19-3 d60fd release-keys" \
+    BuildFingerprint=motorola/scout_g_sys/scout:16/W1VCS36M.14-20-19-3/d60fd:user/release-keys
+
