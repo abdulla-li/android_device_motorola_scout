1849a1eba6abb48f88e519a1739bf557adbabbf7
Notganesh <chimeports@gmail.com>
2026-04-27 11:38:00 +0000

    scout: props: include product.prop
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,6 +159,7 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 BOARD_USES_METADATA_PARTITION := true
 
 # Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
